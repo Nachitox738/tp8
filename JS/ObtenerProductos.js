e388fcd6378782f1d450114eb4f42ed09e45a049
Nachitox738 <ares.ignacio@tecnica7.edu.ar>
2024-09-04 03:06:51 -0300

    cambios2

--- JS/ObtenerProductos.js
+++ JS/ObtenerProductos.js
@@ -7,13 +7,11 @@ const mostrarProductos = (datos) => {
     const contenedor = document.querySelector('#contenedor')
     datos.forEach(dato => {
         productos+=`
-        <div class="card me-3 mt-3" style="width: 100%; max-width: 250px;">
               <img src="${dato.imagen}" class="card-img-top" style="height: 300px; width: 240px;" alt="...">
               <div class="card-body">
                 <h5 class="card-title">${dato.titulo}</h5>
                 <p class="card-text">${dato.descripcion}</p>
                 <a href="#" class="btn btn-primary m-auto">Comprar</a>
-              </div>
             </div>`
     });
     contenedor.innerHTML = productos
